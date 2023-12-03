861efa29416544c47ed80608e81b8c7aaa8551fb
alokartist <btech60020.22@bitmesra.ac.in>
2023-12-03 15:48:00 +0530

    practice

---
+++ javascript/new.js
@@ -0,0 +1,50 @@
+// Practice #1
+console.log("Practicing JavaScript Part-1");
+console.log("\n");
+console.log(" HELLO WORLD PRINTING IN DIFFRENT WAYS ");
+
+console.log("Hello world");
+let a="Hello world";
+console.log(a);
+
+var x="Hello world";
+console.log(x);
+
+const y="hello world";
+console.log(y);
+
+
+// Practice #2
+console.log("Practicing JavaScript Part-2");
+console.log("\n");
+console.log("Primitive Datatype & Non-Primitive Datatype");
+
+// Primitive datatype
+//Total 7 type of primitive data type
+//nn ss bb u "n-number,n-NULL,b-BigInt,b-boolean,s-Symbol,S-string,u-Undefined"
+console.log("\n");
+
+
+
+console.log("primitive datatype");
+//Primitive data type
+let a=null;
+let b= "String";
+let c=BigInt(88);
+let d=67;
+let e=Symbol("javascript");
+let f= undefined;
+
+console.log(a,b,c,d,e,f);
+
+
+//Non-primitive data type
+//also termed as oBjects
+const items={
+    Alok:98,
+    Neha:56,
+    Shalu:54,
+    Naman: 67
+}
+console.log(items["Alok"]);
+console.log(items["Neha"]);
