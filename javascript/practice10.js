51f96dca9008908c8021ee48109a7101668ab2f7
alokartist <btech60020.22@bitmesra.ac.in>
2023-12-05 14:24:03 +0530

    Array & array methods

---
+++ javascript/practice10.js
@@ -0,0 +1,85 @@
+// Practice #10
+
+
+console.log("Practicing JavaScript Part-10");
+console.log("\n");
+console.log("Arrays in javascript");
+
+
+let arr=[90,98,78,89,"alok",null,false];
+console.log(arr);
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]); // Undefined
+
+console.log("The length of the array is - " ,arr.length);
+
+arr[7]=90; // Adding the element in the array
+console.log(arr);
+
+arr[4]=100;  //replacing the array element 
+console.log(arr);
+
+console.log(typeof arr);
+
+
+//Array method:-
+let num=[20,15,14,30];
+
+let b= num.toString();
+console.log(b);
+let c= num.join("-");
+console.log(c);
+r= num.pop();  // r will return the popped element 
+console.log(num,r); // [ 20, 15, 14 ] 30
+
+let g= num.push(60); // Pushing the elements in tha array 
+console.log(num);  // [ 20, 15, 14, 60 ]
+
+let x=num.shift();
+console.log(num); //[15,14,60]   remove the first element 
+console.log(num,x); //[15,14,60] 20
+
+console.log(num.unshift()); // return the length of the array
+
+shiv=[10,9,8,7,6];
+delete shiv[2];    // empty space in the index number 2 
+console.log(shiv); 
+
+
+a1=[10,9,8];
+a2=[1,2,3,4];
+a3=[5,2,3,4];
+let abc= a1.concat(a2,a3);  // mering this all 3 array in one
+console.log(abc);
+
+let xyz=[2,1,5,0];
+let compare =(a,b)=>{
+    return a-b;
+}
+xyz.sort(compare);                //sort in the increasing oder
+console.log(xyz);
+
+let xyz=[2,1,5,0];
+let compare =(a,b)=>{
+    return b-a;
+}
+xyz.sort(compare);                //sort in the decreasing oder
+console.log(xyz);
+
+const number=[1,2,3,4,5,6,7];
+number.splice(2,0,23,24);       // 2-index number, 0-number of element to be removed, 23,24-element to be added 
+console.log(number);
+
+
+
+let num1=[10,20,30,40];
+let num2= num1.slice(2);   // form that idex it will be slice NOTE:-take new variable for running
+console.log(num2);
+
