bb218664a2ab6570d0d44bdd1f09e45cca348f87
alokartist <btech60020.22@bitmesra.ac.in>
2023-12-07 16:41:34 +0530

    searching the DOM

---
+++ practice12.js
@@ -0,0 +1,21 @@
+// Practice #12
+
+
+console.log("Practicing JavaScript Part-12");
+console.log("\n");
+console.log("JavaScript in Browser");
+
+let card1=document.getElementById("Card 1");
+card1.style.color="red"
+
+let card2=document.getElementById("Card 2");
+card2.style.color="blue"
+
+let card3=document.getElementById("Card 3");
+card3.style.color="green"
+
+let this_1=document.querySelector(".this");
+this_1.style.background="red"
+
+console.log(document.getElementsByTagName("a"));
+console.log(document.getElementsByClassName("a"));
