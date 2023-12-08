8fc0e93f3a73079ad025dcf4e8a589475f7c6b45
alokartist <btech60020.22@bitmesra.ac.in>
2023-12-09 03:13:41 +0530

    innerHTML & outerHTML

---
+++ practice14.js
@@ -0,0 +1,21 @@
+//console.log(document.getElementsByTagName("span"))[0];
+// o/p: <span id="id">HEY</span>
+
+//console.dir(document.getElementsByTagName("span"));
+// o/p: span#id
+
+/*
+    id.innerHTML
+->  'HEY'
+    id.outerHTML
+->  '<span id="id">HEY</span>'
+    id.outerHTML="<div>ALOK</div>"
+->  '<div>ALOK</div>'
+
+*/
+
+//test content 
+//console.log(document.body.textContent);
+
+//Hidden property in Html
+//<div hidden>i am hidden</div>
