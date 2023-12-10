bde71856cba8b493de42e40e72722d40a60264b5
alokartist <btech60020.22@bitmesra.ac.in>
2023-12-10 06:21:34 +0530

    Insertion methods & classList(), className in JavaScript

---
+++ practice17.js
@@ -0,0 +1,22 @@
+//Insertion method
+/*
+first.insertAdjacentHTML('beforebegin','<div class="test">beforebegin</div>');
+first.insertAdjacentHTML('beforeend','<div class="test">beforeend</div>');
+first.insertAdjacentHTML('afterbegin','<div class="test">afterbegin</div>');
+first.insertAdjacentHTML('afterend','<div class="test">afterend</div>');
+first.remove() //remove all the elements from DOM of id "first"
+*/
+
+//className & classList
+//first.className="text-dark red"  //color and text will be changed 
+//first.classList  //     o/p->
+/*
+DOMTokenList ['yellow', 'text-dark', value: 'yellow text-dark']
+"yellow"
+"text-dark"
+*/
+
+//first.classList.remove("yellow"); //  o/p-> yellow background will be removed
+//first.classList.add("red"); //  o/p-> Red background will be added
+//first.classList.toggle("red"); //  o/p-> If the background is red then it will be red if not then it willbe added
+first.classList.contains("red");
