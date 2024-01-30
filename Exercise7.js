f21065b48bafc5a63f6fc214e092e8cae0684cf5
alokartist <btech60020.22@bitmesra.ac.in>
2024-01-30 23:31:47 +0530

    Math Magic Calculator

---
+++ Exercise7.js
@@ -0,0 +1,20 @@
+let result = '';
+
+function appendToResult(value) {
+    result += value;
+    document.getElementById('result').value = result;
+}
+
+function clearResult() {
+    result = '';
+    document.getElementById('result').value = result;
+}
+
+function calculateResult() {
+    try {
+        result = eval(result);
+        document.getElementById('result').value = result;
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
