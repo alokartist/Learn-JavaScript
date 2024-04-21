ca8eb3406118794bd65c4d15107bafaaea1a3c79
alokartist <btech60020.22@bitmesra.ac.in>
2024-04-21 11:08:33 +0530

    Regula Falsi Method

---
+++ javascript/Exercise29.js
@@ -0,0 +1,40 @@
+function regulaFalsi() {
+    let func = document.getElementById('function').value;
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    let tol = parseFloat(document.getElementById('tolerance').value);
+
+    let fa = evalFunc(func, a);
+    let fb = evalFunc(func, b);
+
+    if (fa * fb >= 0) {
+        document.getElementById('output').innerText = "f(a) and f(b) must have opposite signs.";
+        return;
+    }
+
+    let iterations = 0;
+    let c;
+
+    while ((b - a) / 2 > tol) {
+        c = (a * fb - b * fa) / (fb - fa);
+        let fc = evalFunc(func, c);
+
+        if (fc === 0) {
+            break;
+        } else if (fa * fc < 0) {
+            b = c;
+            fb = fc;
+        } else {
+            a = c;
+            fa = fc;
+        }
+
+        iterations++;
+    }
+
+    document.getElementById('output').innerText = `Root: ${c.toFixed(6)}\nIterations: ${iterations}`;
+}
+
+function evalFunc(func, x) {
+    return eval(func.replace(/x/g, '(' + x + ')'));
+}
